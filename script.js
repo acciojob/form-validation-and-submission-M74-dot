9e720ed1a8872cfdbcfe882cc2eb21b2abb5af83
Manisha Mali <82174253+M74-dot@users.noreply.github.com>
2024-08-29 16:51:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded',()=>{
+	const chkBtn = document.querySelector('#chk-btn');
+    const nameField = document.querySelector('#name');
+    const submitBtn = document.querySelector('#submit-btn');
+    const form = document.querySelector('#myForm');
+
+	function validateForm(){
+		if(chkBtn.checked && nameField.value.trim()!==''){
+			submitBtn.disabled=false;
+		}else{
+			submitBtn.disabled=true;
+		}
+	}
+	validateForm();
+	chkBtn.addEventListener('change',validateForm);
+	nameField.addEventListener('input',validateForm);
+
+	form.addEventListener('submit',(event)=>{
+		if(!chkBtn.checked || chkBtn.value.trim()!===''){
+			event.preventDefault(); // Prevent form submission
+            alert('Please fill out all required fields and accept the terms and conditions.');
+       
+		}
+	})
+});
